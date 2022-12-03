67843f938ab8e93dd253b62d685e11544e396c93
Gerald White <geraldkwhite10@gmail.com>
2022-12-03 17:21:44 -0600

    initial commit

--- src/get_config.py
+++ src/get_config.py
@@ -1,3 +1,4 @@
+import torch.cuda
 import torchvision.transforms as T
 
 from transformers import GPT2TokenizerFast, GPT2Model
@@ -7,5 +8,6 @@ def get_exp_config():
     config = {'transforms': T.Compose([T.ToTensor(), T.Resize((32, 32))]),
               'tokenizer': GPT2TokenizerFast.from_pretrained("gpt2", pad_token='[PAD]'),
               'imgPath': "IMGPATH",
-              'annPath': "ANNPATH"}
+              'annPath': "ANNPATH",
+              'device': torch.device("cuda:0" if torch.cuda.is_available() else "cpu")}
     return config
