fac4e06c7cdcbd4e08cbe7150dadb90fc377e64c
Gerald White <geraldkwhite10@gmail.com>
2022-12-03 17:27:08 -0600

    initial commit

--- src/get_config.py
+++ src/get_config.py
@@ -7,7 +7,7 @@ from transformers import GPT2TokenizerFast, GPT2Model
 def get_exp_config():
     config = {'transforms': T.Compose([T.ToTensor(), T.Resize((32, 32))]),
               'tokenizer': GPT2TokenizerFast.from_pretrained("gpt2", pad_token='[PAD]'),
-              'imgPath': "IMGPATH",
-              'annPath': "ANNPATH",
+              'imgPath': "/home/geraldkwhite/Image-Caption-Siamese-Learning/mscoco_data/val2017",
+              'annPath': "/home/geraldkwhite/Image-Caption-Siamese-Learning/mscoco_data/annotations/captions_val2017.json",
               'device': torch.device("cuda:0" if torch.cuda.is_available() else "cpu")}
     return config
