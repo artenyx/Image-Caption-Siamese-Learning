e0dce458a186f52df72319680a2c9d4d7348fa0e
Gerald White <geraldkwhite10@gmail.com>
2022-12-03 17:22:32 -0600

    initial commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src import data_loaders as data_loaders, get_config as get_config, models a
 config = get_config.get_exp_config()
 model = models.ImgCapModel(config=config).to(config['device'])
 
-mscoco_loader = data_loaders.get_mscoco_loader()
+mscoco_loader = data_loaders.get_mscoco_loader(config)
 for i, (img, caption) in enumerate(mscoco_loader):
     print(img.shape)
     print(caption)
