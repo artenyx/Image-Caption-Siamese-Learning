7c89a07578dea2d9d2fddb7f26dd8510cafd7e0c
Gerald White <geraldkwhite10@gmail.com>
2022-12-04 17:46:17 -0600

    setting up lr test

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from src.train import train_imgcap_network
+from src.config import get_exp_config
+
+
+def main():
+    lr_list = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1]
+    for lr in lr_list:
+        print("lr:", lr)
+        config = get_exp_config()
+        config['lr'] = lr
+        train_imgcap_network(config=config)
 
 
 if __name__ == "__main__":
-    train_imgcap_network()
+    main()
