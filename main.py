fb7d61176a160c87cb57431830b498ab2bd9fa62
Gerald White <geraldkwhite10@gmail.com>
2022-12-07 10:21:47 -0600

    setting up lr test

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import logging
 import torch
-import time
+from datetime import datetime
 
 from src.experiments import run_img_cap_learning
 from src.config import get_exp_config
 
 
 def main():
-    lr_list = [10**i for i in range(-6, -4)]
-    print(f"Testing learning rates: {lr_list}")
-    print(f"Starting at {time.time()}")
-    for lr in lr_list:
-        config = get_exp_config()
-        config['lr'] = lr
-        run_img_cap_learning(config=config, add_string=str(lr))
+    current_time = datetime.now().strftime("%H:%M:%S")
+    print(f"Starting at {current_time}")
+    run_img_cap_learning()
 
 
 if __name__ == "__main__":
